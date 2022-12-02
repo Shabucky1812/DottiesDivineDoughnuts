8581fa4bcd15c7eca30473eac75142f99a480141
Shaun Buck <shaunbuck1812@gmail.com>
2022-12-02 12:52:27 +0000

    Add main function structure

--- run.py
+++ run.py
@@ -11,3 +11,18 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('dotties_divine_doughnuts')
+
+
+def main():
+    """
+    Welcomes the user and explains functionality of
+    the choices provided. The user can choose one of three
+    options:
+        1. Log a current customer's order and calulcate their total price.
+        2. Edit the menu (Change/Remove/Add choices, Change prices).
+        3. View some analytics about recent orders.
+    """
+    print('Welcome to \'Dottie\'s Divine Doughnuts\' services application!')
+
+
+main()
